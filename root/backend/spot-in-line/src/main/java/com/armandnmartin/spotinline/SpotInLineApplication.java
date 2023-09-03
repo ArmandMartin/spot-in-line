bc03fc451643976c5f3656cca85710493b7db725
root <root@Beltalowda.localdomain>
2023-09-03 19:46:37 -0400

    first commit

---
+++ root/backend/spot-in-line/src/main/java/com/armandnmartin/spotinline/SpotInLineApplication.java
@@ -0,0 +1,13 @@
+package com.armandnmartin.spotinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotInLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotInLineApplication.class, args);
+	}
+
+}
---
+++ root/backend/spot-in-line/src/test/java/com/armandnmartin/spotinline/SpotInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armandnmartin.spotinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotInLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
